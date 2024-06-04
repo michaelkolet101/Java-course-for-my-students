4ff4c2837e1c18f64c6721792944d881004bdae8
michaelkolet101 <michael24kolet@gmail.com>
2024-06-04 09:59:44 +0300

    update

---
+++ eleventh_grade/Preparation_for_matriculation/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!");
+		
+		
+		
+		if (5 > 3){
+			System.out.print("BALKKAUUUA TEST");
+		}
+	}
+
+}
